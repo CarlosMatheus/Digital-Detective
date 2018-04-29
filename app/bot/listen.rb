4f65862085337fc944ec6da2aa6841c1a1b3839b
Eric Moreira <ericpqmor@gmail.com>
2018-04-28 22:26:41 -0300

    FB messenger setup ready

---
+++ app/bot/listen.rb
@@ -0,0 +1,11 @@
+# app/bot/listen.rb
+
+require "facebook/messenger"
+
+include Facebook::Messenger
+
+Facebook::Messenger::Subscriptions.subscribe(access_token: ENV["ACCESS_TOKEN"])
+
+Bot.on :message do |message|
+  puts "got your message!"
+end
