43cbef1b174f27f6986556212747ce387c845c71
giovane <giovane.tomaz@redealumni.com>
2018-04-29 10:10:41 -0300

    Add tapioca to facts

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,9 @@
 
 require 'open-uri'
 
+Story.delete_all
+New.delete_all
+
 facts_xml = Nokogiri::XML(open("http://pox.globo.com/rss/g1/e-ou-nao-e/"))
 
 facts_xml.xpath('//item').each do |item|
@@ -24,6 +27,7 @@ facts_xml.xpath('//item').each do |item|
     fake: fake, subject_id: subject.id)
 
   story.fact_id = fact.id
+  story.save
   fact.save
 
 end
@@ -42,6 +46,25 @@ news_xml.xpath('//item').each do |item|
   neu = New.create(link: item.xpath('guid').text, text: text, subject_id: subject.id)
 
   story.new_id = neu.id
+  story.save
   neu.save
 
 end
+
+
+# Mandioca mata
+story = Story.create()
+
+subject = Subject.create(title: 'Tapioca pode matar? Não é verdade!', story_id: story.id)
+
+link = 'https://g1.globo.com/e-ou-nao-e/noticia/tapioca-pode-matar-nao-e-verdade.ghtml'
+
+html = Nokogiri::HTML(open(link))
+
+text = html.xpath('//p').text
+
+fact = Fact.create(link: link, text: text, subject_id: subject.id)
+
+story.fact_id = fact.id
+story.save
+fact.save
