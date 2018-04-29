49d1b86cc2c9ae6a18f4d2bcd89b7da5b345abe5
giovane <giovane.tomaz@redealumni.com>
2018-04-29 06:48:07 -0300

    Previiieewww :tada:

--- app/controllers/query_controller.rb
+++ app/controllers/query_controller.rb
@@ -4,18 +4,48 @@ class QueryController < ApplicationController
   def index
 
     text = get_text(params[:query])
-    news_list = NewsService.get_news;
-    facts_list = FactsService.get_facts;
+    news_list = NewsService.get_news
+    facts_list = FactsService.get_facts
 
-    story = NLPService.check_content(text, news_list, facts_list);
+    story = NLPService.check_content(text, news_list, facts_list)
 
-    render json: {
-      story: story
+    if story.present? and story != 'to do'
+      if story.is_a? Fact
+        if story.fake
+          result_image = get_image(NLPService::CHECK_RESPONSE[:fake])
+          @true_or_fake = "É Fake!"
+        else
+          @true_or_fake = "É Verdade!"
+          result_image = get_image(NLPService::CHECK_RESPONSE[:fact])
+        end
+      else
+        result_image = get_image(NLPService::CHECK_RESPONSE[:news])
+        @true_or_fake = "Não sabemos afirmar"
+      end
+      @link = story.link
+    else
+      result_image = get_image(NLPService::CHECK_RESPONSE[:dunno])
+      @true_or_fake = "Nao encontramos nada =/ Mas te avisamos quando encontrar!"
+      @link = nil
+    end
+
+    render locals: {
+      true_or_fake: @true_or_fake,
+      link: @link,
+      result_image: result_image
     }
 
   end
 
   private
+
+  def get_image(code)
+    NLPService::CHECK_RESPONSE.each do |k, v|
+      if v == code
+        return  k.to_s
+      end
+    end
+  end
 
   def get_text(query)
 
