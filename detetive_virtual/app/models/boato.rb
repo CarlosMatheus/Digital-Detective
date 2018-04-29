c63e964cd5efdbbff156fd05a86ddd1af77dad57
Carlos Matheus <carlosmatheusbs@gmail.com>
2018-04-28 22:06:26 -0300

    Create hash algorithm

---
+++ detetive_virtual/app/models/boato.rb
@@ -0,0 +1,5 @@
+class Boato < ApplicationRecord
+  searchkick
+
+  
+end
--- detetive_virtual/app/services/nlp_service.rb
+++ detetive_virtual/app/services/nlp_service.rb
@@ -19,27 +19,47 @@ class NLPService
     dunno: 3
   }
 
-  #def self.check_content(text, news_list, facts_list)
+  # def self.check_content(text, news_list, facts_list)
   def self.check_content()
   	CHECK_RESPONSE[:fake]
 
-  	#binding.pry
+ #  	#binding.pry
 
-	# Instantiates a client
-	language = Google::Cloud::Language.new 
+	# # Instantiates a client
+	# language = Google::Cloud::Language.new 
 
-	# The text to analyze
-	text = "Hello, world!"
+	# # The text to analyze
+	text = "Segundo os bombeiros, as vítimas estavam indo para o trabalho. O teto de carro do carro foi aberto para retirar os ocupantes. Carro capota em avenida perto de shopping em Campo Grande Um homem de 24 e uma mulher de 30 anos saíram ilesos depois do carro onde estavam ter batido em um poste, no portão de uma empresa e capotar na avenida Ernesto Geisel, em Campo Grande, quando iam para o trabalho na manhã deste sábado (28). Segundo o Corpo de Bombeiros foi necessário usar o desencarcerador para retirar as vítimas de dentro do veículo, mas apesar disso, sofreram apenas arranhões. O motorista disse aos policiais que perdeu o controle do carro após ser fechado por outro condutor. Além do casal, o dono da oficina que teve o portão danificado também ficou assustado quando chegou para trabalhar. Na hora que eu cheguei para abrir a oficina, o carro estava tombado aqui na frente e fique assustado quando vi, disse Márcio Cardozo. Bombeiros abriram teto do carro para retirar as vítimas Evelyn Souza/TV Morena"
 
-	# Detects the sentiment of the text
-	#response = language.analyze_sentiment content: text, type: :PLAIN_TEXT
-	response = language.analyze_entities content: text, type: :PLAIN_TEXT
+	# # Detects the sentiment of the text
+	# #response = language.analyze_sentiment content: text, type: :PLAIN_TEXT
+	# response = language.analyze_entities content: text, type: :PLAIN_TEXT, language:"pt"
 
-	# Get document sentiment from response
-	sentiment = response.document_sentiment
+	# # Get document sentiment from response
+	# sentiment = response.document_sentiment
 
-	puts "Text: #{text}"
-	puts "Score: #{sentiment.score}, #{sentiment.magnitude}"
+	# puts "Text: #{text}"
+	# puts "Score: #{sentiment.score}, #{sentiment.magnitude}"
+	letter_to_remove = [",", ".","!","'","\"",":", ";", "?","[","]","{","}","(",")","/"]
+	for letter in letter_to_remove
+		text = text.remove(letter)
+	end
+	words = text.split(" ")
+	filtered_words = []
+	for word in words
+		filtered_words.push(word.mb_chars.downcase.to_s)
+	end
+	dic = {}
+	for word in filtered_words
+		if dic[word] == nil
+			dic[word]=1
+		else
+			dic[word] += 1
+		end
+	end
+
+	puts dic
+
   end
 
 end
